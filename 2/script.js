531b5c5cdc0eb55d3e1f246db44e5e68cdc395f3
lukamyson <aldiyardadabayev35@gmail.com>
2022-03-10 20:27:53 +0500

    2

--- 2/script.js
+++ 2/script.js
@@ -4,7 +4,7 @@ let num1 = +prompt('Birinchi sonni kiriting'),
 if ((num1 > num2 && num1 < num3) || (num1 < num2 && num1 > num3)) {
     alert(`Siz ${num1} ${num2} ${num3} kiritdiz\nBirinchi son ${num1} ortancha`);
 } else if((num2 > num1 && num2 < num3) || (num2 < num1 && num2 > num3)){
-    alert(`Siz ${num1} ${num2} ${num3} kiritdiz\Ikkinchi son ${num1} ortancha`);
+    alert(`Siz ${num1} ${num2} ${num3} kiritdiz\nIkkinchi son ${num2} ortancha`);
 } else if((num3 > num2 && num3 < num1) || (num3 < num2 && num3 > num1)){
-    alert(`Siz ${num1} ${num2} ${num3} kiritdiz\Uchinchi son ${num1} ortancha`);
+    alert(`Siz ${num1} ${num2} ${num3} kiritdiz\nUchinchi son ${num3} ortancha`);
 }
