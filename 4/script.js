3aebff7e4db65cf03741fb192c05a181980acb19
lukamyson <aldiyardadabayev35@gmail.com>
2022-03-17 19:09:40 +0500

    4

--- 4/script.js
+++ 4/script.js
@@ -1,12 +1,14 @@
-let total = "";
+let total = '';
 for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-        if (i == j) {
-            total += '🎱'
+    for (let j = 8; j > 0; j--) {
+        if (i == j - 1 || j == 8 || j == 1 || i == 0 || i == 7) {
+            total += '🎱';
         } else {
-            total += '🏐'
+            total += '🏐';
         }
     }
     console.log(total);
-    total = "";
+    total = '';
 }
+
+
